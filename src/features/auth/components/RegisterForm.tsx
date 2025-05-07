f78599e85452b9a04a1748e31e8aa03402241c61
Aa <aa@example.com>
2025-05-07 13:57:16 +0300

    with prettier

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,6 +1,26 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
 import { AuthWrapper } from '@/features/auth/components/AuthWrapper'
+import { RegisterSchema, TypeRegisterSchema } from '@/features/auth/schemes'
 
 export function RegisterForm() {
+	const form = useForm<TypeRegisterSchema>({
+		resolver: zodResolver(RegisterSchema),
+		defaultValues: {
+			name: '',
+			email: '',
+			password: '',
+			passwordRepeat: ''
+		}
+	})
+
+	const onSubmit = async (values: TypeRegisterSchema) => {
+		console.log(values)
+	}
+
 	return (
 		<AuthWrapper
 			heading='Register'
