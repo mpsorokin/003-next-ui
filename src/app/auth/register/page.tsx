2f9ca38d6296dfa7b837eccf33f1932c9d797d16
Aa <aa@example.com>
2025-05-07 12:42:23 +0300

    with prettier

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next'
 
+import { RegisterForm } from '@/features/auth/components/RegisterForm'
+
 export const metadata: Metadata = {
 	title: 'Create account',
 	description: ''
 }
 
 export default function RegisterPage() {
-	return <div></div>
+	return <RegisterForm />
 }
---
+++ src/features/auth/components/AuthWrapper.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import { PropsWithChildren } from 'react'
+
+import {
+	Button,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from '@/shared/components/ui'
+
+interface AuthWrapperProps {
+	heading: string
+	description?: string
+	backButtonLabel?: string
+	backButtonHref?: string
+}
+
+export function AuthWrapper({
+	children,
+	heading,
+	description,
+	backButtonHref,
+	backButtonLabel
+}: PropsWithChildren<AuthWrapperProps>) {
+	return (
+		<Card className='w-[400px]'>
+			<CardHeader className='space-y-2'>
+				<CardTitle>{heading}</CardTitle>
+				{description && (
+					<CardDescription>{description}</CardDescription>
+				)}
+			</CardHeader>
+			<CardContent>{children}</CardContent>
+			<CardFooter>
+				{backButtonLabel && backButtonHref && (
+					<Button variant='link' className='w-full font-normal'>
+						<Link href={backButtonHref}>{backButtonLabel}</Link>
+					</Button>
+				)}
+			</CardFooter>
+		</Card>
+	)
+}
