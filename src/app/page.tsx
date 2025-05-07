66936bf975506694d276c899414881ba3c53671f
Aa <aa@example.com>
2025-05-07 11:38:48 +0300

    with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
+import { buttonVariants } from '@/shared/components/ui'
+
 export default function Home() {
-	return <div>Home page</div>
+	return (
+		<div className='space-y-5 text-center'>
+			<h1 className='text-4xl font-bold'>Home page</h1>
+			<Link href='/auth/login' className={buttonVariants()}>
+				Login Account
+			</Link>
+		</div>
+	)
 }
