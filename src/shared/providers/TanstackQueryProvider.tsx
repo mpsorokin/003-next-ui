59ef68ac475768f4caecbe18c54e8d14a6a2b299
Aa <aa@example.com>
2025-05-07 11:30:09 +0300

    with prettier

--- src/shared/providers/TanstackQueryProvider.tsx
+++ src/shared/providers/TanstackQueryProvider.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import {PropsWithChildren, useState} from "react";
-import {QueryClientProvider} from "@tanstack/react-query";
-import {QueryClient} from "@tanstack/query-core";
+import { QueryClient } from '@tanstack/query-core'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { PropsWithChildren, useState } from 'react'
 
-export function TanstackQueryProvider({children}: PropsWithChildren<unknown>) {
-    const [client] = useState(new QueryClient({
-        defaultOptions: {
-            queries: {
-                refetchOnWindowFocus: false,
-            }
-        }
-    }));
-    return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+export function TanstackQueryProvider({
+	children
+}: PropsWithChildren<unknown>) {
+	const [client] = useState(
+		new QueryClient({
+			defaultOptions: {
+				queries: {
+					refetchOnWindowFocus: false
+				}
+			}
+		})
+	)
+	return <QueryClientProvider client={client}>{children}</QueryClientProvider>
 }
