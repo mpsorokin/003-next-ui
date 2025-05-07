483fb9203d64633c0fca20bb6ca0a86483b4d0f5
Aa <aa@example.com>
2025-05-07 11:32:39 +0300

    with prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
+import { MainProvider } from '@/shared/providers'
+
 import '../shared/styles/globals.css'
 
 const geistSans = Geist({
@@ -28,7 +30,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<MainProvider>{children}</MainProvider>
 			</body>
 		</html>
 	)
