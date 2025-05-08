4ba924bf0c0812cdecdcefad62fea0ca206cdc52
Aa <aa@example.com>
2025-05-08 11:21:36 +0300

    with prettier

--- src/features/auth/hooks/useRegisterMutation.ts
+++ src/features/auth/hooks/useRegisterMutation.ts
@@ -1,14 +1,23 @@
 import { useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
 import { TypeRegisterSchema } from '@/features/auth/schemes'
 import { authService } from '@/features/auth/services/auth.service'
+
+import { toastMessageHandler } from '@/shared/utils/toast-message-handler'
 
 export function useRegisterMutation() {
 	const {} = useMutation({
 		mutationKey: ['register user'],
 		mutationFn: ({ values }: { values: TypeRegisterSchema }) =>
 			authService.register(values),
-		onSuccess() {},
-		onError() {}
+		onSuccess() {
+			toast.success('Register successful', {
+				description: 'Proof the mail. Mail was sent to mailbox'
+			})
+		},
+		onError(error: Error) {
+			toastMessageHandler(error)
+		}
 	})
 }
