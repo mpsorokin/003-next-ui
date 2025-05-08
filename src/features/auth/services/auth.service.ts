089de8a624bebf480be7130c6b2a54935aa2b81c
Aa <aa@example.com>
2025-05-08 11:00:47 +0300

    with prettier

---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { TypeLoginSchema, TypeRegisterSchema } from '@/features/auth/schemes'
+import { IUser } from '@/features/auth/services/user.types'
+
+import { api } from '@/shared/api/instance.api'
+
+class AuthService {
+	async register(body: TypeRegisterSchema) {
+		const response = api.post<IUser>('auth/register', body)
+
+		return response
+	}
+
+	async login(body: TypeLoginSchema) {
+		const response = api.post<IUser>('auth/login', body)
+
+		return response
+	}
+
+	async logout() {
+		const response = await api.post('auth/logout')
+
+		return response
+	}
+}
+
+export const authService = new AuthService()
---
+++ src/features/auth/services/user.types.ts
@@ -0,0 +1,34 @@
+export enum UserRole {
+	REGULAR = 'REGULAR',
+	ADMIN = 'ADMIN'
+}
+
+export enum AuthMethod {
+	CREDENTIALS = 'CREDENTIALS'
+}
+
+export interface IAccount {
+	id: string
+	createdAt: string
+	updatedAt: string
+	type: string
+	provider: string
+	refreshToken: string
+	accessToken: string
+	expiresAt: number
+	userId: string
+}
+
+export interface IUser {
+	id: string
+	createdAt: string
+	updatedAt: string
+	email: string
+	password: string
+	displayName: string
+	picture: string
+	role: UserRole
+	isVerified: boolean
+	method: AuthMethod
+	accounts: IAccount[]
+}
