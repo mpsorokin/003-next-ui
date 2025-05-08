888799e2dd251131a3e2a0e8b30c1990ab44efe2
Aa <aa@example.com>
2025-05-08 11:27:29 +0300

    with prettier

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -9,120 +9,125 @@ import { useRegisterMutation } from '@/features/auth/hooks/useRegisterMutation'
 import { RegisterSchema, TypeRegisterSchema } from '@/features/auth/schemes'
 
 import {
-	Button,
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-	Input
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input
 } from '@/shared/components/ui'
 
 export function RegisterForm() {
-	const form = useForm<TypeRegisterSchema>({
-		resolver: zodResolver(RegisterSchema),
-		defaultValues: {
-			name: '',
-			email: '',
-			password: '',
-			passwordRepeat: ''
-		}
-	})
+  const form = useForm<TypeRegisterSchema>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordRepeat: ''
+    }
+  })
 
-	const { register, isLoadingRegister } = useRegisterMutation()
+  const { register, isLoadingRegister } = useRegisterMutation()
 
-	const onHandleSubmit = async (values: TypeRegisterSchema) => {
-		console.log(values)
-		register({ values })
-		toast.info('Register successfully.')
-	}
+  const onHandleSubmit = async (values: TypeRegisterSchema) => {
+    console.log(values)
+    register({ values })
+    toast.info('Register successfully.')
+  }
 
-	return (
-		<AuthWrapper
-			heading='Register'
-			description='Input email and password'
-			backButtonLabel='Already have an account?'
-			backButtonHref='/auth/login'
-		>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onHandleSubmit)}
-					className='grid gap-2 space-y-2'
-				>
-					<FormField
-						control={form.control}
-						name='name'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Name</FormLabel>
-								<FormControl>
-									<Input
-										disabled={isLoadingRegister}
-										placeholder='John Doe'
-										{...field}
-									></Input>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+  return (
+    <AuthWrapper
+      heading='Register'
+      description='Input email and password'
+      backButtonLabel='Already have an account?'
+      backButtonHref='/auth/login'
+    >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onHandleSubmit)}
+          className='grid gap-2 space-y-2'
+        >
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoadingRegister}
+                    placeholder='John Doe'
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-					<FormField
-						control={form.control}
-						name='email'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='test@mail.com'
-										type='email'
-										{...field}
-									></Input>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='password'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Password</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='***'
-										type='password'
-										{...field}
-									></Input>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoadingRegister}
+                    placeholder='test@mail.com'
+                    type='email'
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoadingRegister}
+                    placeholder='******'
+                    type='password'
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-					<FormField
-						control={form.control}
-						name='passwordRepeat'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Password Repeat</FormLabel>
-								<FormControl>
-									<Input
-										placeholder='***'
-										type='password'
-										{...field}
-									></Input>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type='submit'>Create account</Button>
-				</form>
-			</Form>
-		</AuthWrapper>
-	)
+          <FormField
+            control={form.control}
+            name='passwordRepeat'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password Repeat</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoadingRegister}
+                    placeholder='******'
+                    type='password'
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isLoadingRegister} type='submit'>
+            Create account
+          </Button>
+        </form>
+      </Form>
+    </AuthWrapper>
+  )
 }
